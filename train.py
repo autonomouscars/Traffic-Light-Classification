d97962fd66a4c0feb4732d5a15d2824fe99fa71c
sonnet_rtx2080 <info@sonnet.ai>
2020-01-19 12:18:27 +0900

    Config file changed to set 512x512 image resize.

--- train.py
+++ train.py
@@ -46,7 +46,7 @@ import json
 import os
 import tensorflow as tf
 
-from object_detection import trainer
+from object_detection.legacy import trainer
 from object_detection.builders import input_reader_builder
 from object_detection.builders import model_builder
 from object_detection.utils import config_util
